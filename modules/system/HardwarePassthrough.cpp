522d5d95971e36596e35c219d8a420371d4aba5a
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:51:49 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/system/HardwarePassthrough.cpp
@@ -0,0 +1,6 @@
+#include "HardwarePassthrough.hpp"
+#include <iostream>
+HardwarePassthrough::HardwarePassthrough() {}
+void HardwarePassthrough::execute() {
+    std::cout << "Feature HardwarePassthrough active" << std::endl;
+}
