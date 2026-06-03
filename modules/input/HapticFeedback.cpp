1d4a9e9a339838046846bf17952f3605a5328e5d
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:52:41 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/input/HapticFeedback.cpp
@@ -0,0 +1,6 @@
+#include "HapticFeedback.hpp"
+#include <iostream>
+HapticFeedback::HapticFeedback() {}
+void HapticFeedback::execute() {
+    std::cout << "Feature HapticFeedback active" << std::endl;
+}
