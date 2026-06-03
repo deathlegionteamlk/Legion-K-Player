22bcbacdba34a68ff814208b87ca9131258bc7d1
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 19:54:06 +0530

    Push Full Client Code by demo x hexa

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,45 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-#include <iostream>
+#include "core/KVirtualMachine.hpp"
+#include "ui/KInterface.hpp"
 #include <vector>
-#include "KEngine.hpp"
-#include "UI.hpp"
-
 int main(int argc, char* argv[]) {
     if (argc > 1 && std::string(argv[1]) == "--test") {
-        KEngine engine;
-        std::vector<Instruction> prog = {
-            {OpCode::LDI, 0, 0, 10},
-            {OpCode::LDI, 1, 0, 20},
-            {OpCode::ADD, 0, 1, 0},
-            {OpCode::PRT, 0, 0, 0},
-            {OpCode::HALT, 0, 0, 0}
+        KVirtualMachine vm;
+        std::vector<K_INS> boot_image = {
+            {K_OP::REG_LOAD, 0, 0, 100},
+            {K_OP::REG_LOAD, 1, 0, 200},
+            {K_OP::ALU_ADD,  0, 1, 0},
+            {K_OP::OUT_PRT,  0, 0, 0},
+            {K_OP::SYS_HALT, 0, 0, 0}
         };
-        engine.loadProgram(prog);
-        engine.run();
+        vm.boot(boot_image);
+        vm.cycle();
         return 0;
     }
-
     if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) return 1;
-    if (TTF_Init() == -1) return 1;
-
-    SDL_Window* window = SDL_CreateWindow("Legion K Player - Full Version", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
-    if (!window) return 1;
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) return 1;
-
-    UI ui(renderer);
-    KEngine engine;
-
-    bool quit = false;
-    SDL_Event e;
-
-    while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) quit = true;
-            ui.handleEvent(e, engine);
+    SDL_Window* win = SDL_CreateWindow("Legion K Player | Client", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+    if (!win) return 1;
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (!ren) return 1;
+    KVirtualMachine vm;
+    KInterface ui(ren);
+    bool run = true;
+    SDL_Event ev;
+    while (run) {
+        while (SDL_PollEvent(&ev) != 0) {
+            if (ev.type == SDL_QUIT) run = false;
+            ui.process(ev, vm);
         }
-
-        SDL_SetRenderDrawColor(renderer, 10, 20, 40, 255);
-        SDL_RenderClear(renderer);
-
-        ui.render(engine);
-
-        SDL_RenderPresent(renderer);
+        SDL_SetRenderDrawColor(ren, 5, 10, 25, 255);
+        SDL_RenderClear(ren);
+        ui.draw(vm);
+        SDL_RenderPresent(ren);
         SDL_Delay(16);
     }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
-    IMG_Quit();
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
     SDL_Quit();
-
     return 0;
 }
