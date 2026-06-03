168d52b9f3aebbfabc3ff0963bb02be96833fab5
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:52:00 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/system/HardwarePassthrough.hpp
@@ -0,0 +1,9 @@
+#ifndef HARDWAREPASSTHROUGH_HPP
+#define HARDWAREPASSTHROUGH_HPP
+#include <string>
+class HardwarePassthrough {
+public:
+    HardwarePassthrough();
+    void execute();
+};
+#endif
