f441ebd3e896b57eb176c8dd24d6a9682b8ab2a1
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:52:57 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/input/ShortcutManager.cpp
@@ -0,0 +1,6 @@
+#include "ShortcutManager.hpp"
+#include <iostream>
+ShortcutManager::ShortcutManager() {}
+void ShortcutManager::execute() {
+    std::cout << "Feature ShortcutManager active" << std::endl;
+}
