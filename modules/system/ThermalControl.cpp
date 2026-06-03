d9fa3cf428a9bdf5f428800a5bef3234e4e09dfa
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:52:20 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/system/ThermalControl.cpp
@@ -0,0 +1,6 @@
+#include "ThermalControl.hpp"
+#include <iostream>
+ThermalControl::ThermalControl() {}
+void ThermalControl::execute() {
+    std::cout << "Feature ThermalControl active" << std::endl;
+}
