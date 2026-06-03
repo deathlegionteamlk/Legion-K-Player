df759d23bc4d651b923617b891d921a1170177c5
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:52:51 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/input/MouseGestures.cpp
@@ -0,0 +1,6 @@
+#include "MouseGestures.hpp"
+#include <iostream>
+MouseGestures::MouseGestures() {}
+void MouseGestures::execute() {
+    std::cout << "Feature MouseGestures active" << std::endl;
+}
