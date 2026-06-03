5ff5054fa0c30fa98f936d64e92262d6eb84a22d
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 19:54:13 +0530

    Push Full Client Code by demo x hexa

---
+++ include/core/Types.hpp
@@ -0,0 +1,10 @@
+#ifndef K_TYPES_HPP
+#define K_TYPES_HPP
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <memory>
+using k_addr = uint32_t;
+using k_word = int32_t;
+using k_reg = uint8_t;
+#endif
