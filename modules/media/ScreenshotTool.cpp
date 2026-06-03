fea219d0d34931d2f6b397405fbd9caba421f90b
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:54:25 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/media/ScreenshotTool.cpp
@@ -0,0 +1,6 @@
+#include "ScreenshotTool.hpp"
+#include <iostream>
+ScreenshotTool::ScreenshotTool() {}
+void ScreenshotTool::execute() {
+    std::cout << "Feature ScreenshotTool active" << std::endl;
+}
