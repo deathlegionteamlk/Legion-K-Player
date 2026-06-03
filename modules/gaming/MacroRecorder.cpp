bfd9f40e5d1f975b6f5de672975df3540d76b466
Death Legion Team <bbhhexa@gmail.com>
2026-06-03 20:53:32 +0530

    Push Massive 100+ Feature Update by demo x hexa

---
+++ modules/gaming/MacroRecorder.cpp
@@ -0,0 +1,6 @@
+#include "MacroRecorder.hpp"
+#include <iostream>
+MacroRecorder::MacroRecorder() {}
+void MacroRecorder::execute() {
+    std::cout << "Feature MacroRecorder active" << std::endl;
+}
